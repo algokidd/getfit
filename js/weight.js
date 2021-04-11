2346e7eb11e694ba14d7129cae7e9050119c4d6e
algokidd <shubham.21920081@viit.ac.in>
2021-04-11 17:49:42 +0530

    fixed weight.js on idweight.html

--- js/weight.js
+++ js/weight.js
@@ -1,18 +1,18 @@
-var $ = function (id) { return document.getElementById(id) };
+var abc = function (id) { return document.getElementById(id) };
 function bweight() {
-    var sex = $('gender').value;
-    var height = $('height').value;
+    var sex = abc('gender').value;
+    var height = abc('height').value;
     if (height == null || height.length == 0) {
-        $('ibw').value = 'Pl. enter data.'
+        abc('ibw').value = 'Pl. enter data.'
     } else {
-        $('ibw').value = ''
+        abc('ibw').value = ''
     } if (height > 0 && sex == 'm') {
-        $('ibw').value = Math.round(56.2 * 1 + (height * .39 - 152.4 * .39) * 1.41) * 1 + ' Kgs'
+        abc('ibw').value = Math.round(56.2 * 1 + (height * .39 - 152.4 * .39) * 1.41) * 1 + ' Kgs'
     } else if (height > 0 && sex == 'f') {
-        $('ibw').value = Math.round(53.1 * 1 + (height * .39 - 152.4 * .39) * 1.36) * 1 + ' Kgs'
+        abc('ibw').value = Math.round(53.1 * 1 + (height * .39 - 152.4 * .39) * 1.36) * 1 + ' Kgs'
 
     } if (height > 0) {
-        $('ibw1').value = Math.round(18.5 * (height * height / 10000)) + ' Kgs-' + Math.round(25 * (height * height / 10000)) + ' Kgs'
+        abc('ibw1').value = Math.round(18.5 * (height * height / 10000)) + ' Kgs-' + Math.round(25 * (height * height / 10000)) + ' Kgs'
     } 
 }
 
